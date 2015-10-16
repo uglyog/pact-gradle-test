c50d9605581ec3b97d214c43679f3a724ee447de
Ronald Holshausen <ronald.holshausen@ioof.com.au>
2015-10-16 11:17:32 +1100

    test for matchers

--- server.sh
+++ server.sh
@@ -1,1 +1,3 @@
-while true; do { echo -e 'HTTP/1.1 404\r\nDate: Fri, 24 Apr 2015 18:47:33 GMT\r\nContent-Type: application/json\r\nContent-Length: 95\r\n\r\n{"errors":[{"code":"2","message":"cannot be blank","field-name":"token","invalid-value":" "}]}'; } | nc -l -p 8084; done
+#!/usr/bin/env bash
+while true; do { echo -e 'HTTP/1.1 200\r\nDate: Fri, 24 Apr 2015 18:47:33 GMT\r\nContent-Type: application/json\r\nContent-Length: 126\r\n\r\n{"activities": [\n{\n"name": "bob",\n"description": "test description"\n},\n{\n"name": "fred",\n"description": "description 2"\n}\n]\n}'; } | nc -l -p 8084; done
+
