4f7a9bb1b2caf7cd770a9a0d6af049c3c4a48c5e
Ron Holshausen <Ron.Holshausen@team.telstra.com>
2016-02-02 11:46:10 +1100

    added junit provider test

--- src/main/resources/Ratpack.groovy
+++ src/main/resources/Ratpack.groovy
@@ -1,4 +1,5 @@
 import groovy.json.JsonOutput
+import ratpack.http.TypedData
 
 import static ratpack.groovy.Groovy.ratpack
 
@@ -7,6 +8,26 @@ ratpack {
   handlers {
 
     get('activities') {
+      response.contentType('application/json')
+      render(JsonOutput.toJson([
+        activities: [
+          [name: "Bob", "description": "Description of bob"],
+          [name: "Fred", "description": "Description of Fred"],
+        ]
+      ]))
+    }
+
+    post('query') {
+      println "path=${request.path}"
+      println "contentType=${request.contentType}"
+      println "headers:"
+      request.headers.names.each {
+        println "    $it=[${request.headers.get(it)}]"
+      }
+      request.getBody().then { TypedData data ->
+        println "body=[${data.text}]"
+      }
+
       response.contentType('application/json')
       render(JsonOutput.toJson([
         activities: [
---
+++ src/test/java/com/github/uglyog/pactgradletest/PactJUnitTest.java
@@ -0,0 +1,19 @@
+package com.github.uglyog.pactgradletest;
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import org.junit.runner.RunWith;
+
+@RunWith(PactRunner.class)
+@Provider("Activity Service")
+@PactBroker(host = "${pactbroker.url:localhost}", port = "80")
+public class PactJUnitTest {
+
+  @TestTarget
+  public final Target target = new HttpTarget(5050);
+
+}
