8e0d6f66c79d67e37d3bd33e4c76cba4f3517c7d
Ronald Holshausen <uglyog@gmail.com>
2017-05-12 10:04:21 +1000

    tested publishing pact verifications

--- src/main/resources/Ratpack.groovy
+++ src/main/resources/Ratpack.groovy
@@ -3,6 +3,8 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import ratpack.handling.RequestLogger
 import ratpack.http.TypedData
+//import ratpack.pac4j.RatpackPac4j
+//import org.pac4j.http.client.direct.DirectBasicAuthClient
 
 import static ratpack.groovy.Groovy.ratpack
 
@@ -13,6 +15,8 @@ ratpack {
   handlers {
 
     all RequestLogger.ncsa(logger)
+
+//    all RatpackPac4j.authenticator(new DirectBasicAuthClient())
 
     get {
       response.contentType('application/json')
@@ -25,7 +29,7 @@ ratpack {
       response.contentType('application/json')
       render(JsonOutput.toJson([
         activities: [
-//          [name: "Bob", "description": "Description of bob"],
+          [name: "Bob", "description": "Description of bob"],
           [name: "Fred", "description": 100],
         ]
       ]))
@@ -108,6 +112,390 @@ ratpack {
           ]
         ]
       ]))
+    }
+
+    get('some/url/client') {
+      response.contentType('application/json; charset=UTF-8')
+      response.status(400)
+      response.headers.add('X-TEST', 'test')
+      render('''[
+        {
+          "accessTokenValiditySeconds": 2047531805,
+          "allowedScopes": [
+                        100,
+                        "HWQEabGJImGSEkp",
+                        "LaIhYNueeqcDQfm",
+                        "SCxIXIFATJEIvit",
+                        "SSqGVJiwXycnHZO",
+                        "iPpENLhFTbXGpTt",
+                        "qmKXLMsEdaXdyvn",
+                        "uJuBiZBGiToWRYK",
+                        "xEtffZzKEQdXNBv",
+                        "ySWoarvfBHBAOmA"
+          ],
+
+          "autoApprove": true,
+          "clientSecret": "$2a$10$6W5venYTPJvN56SoCVISzOAS9NRgnvkQLWI6XKUTgpHD6Q75d9BRu",
+          "contactEmail": "jsFDnxjMmKedPsM",
+          "contactFirstName": "CnubJIiONQshfju",
+          "contactLastName": "dSzktEWSSSCprRV",
+          "description": "DDnerbtCJcXJdSJ",
+          "grantTypes": [
+                          "AZyZGSnloQEkXrZ",
+                          "BNgxGvVgawObCVA",
+                          "BjCRlTHkIrknhYY",
+                          "LHMMMDnYcvuLBBr",
+                          "OIrutyPdvoSFBEK",
+                          "QvCZUUeKLSXfSwP",
+                          "WtQmSJdnWutzEFu",
+                          "tzKRkWouoUtnZYH",
+                          "vYhaZOcFlrIscRU",
+                          "wktIDvVUQJUehFR"
+            ],
+                  "id": "9daaad0a-8a2d-4e73-a963-fa1625cec110",
+                  "name": "name",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+          "redirectUris": [
+                          "JSdQvUdokPTlodl",
+                          "JpEMvpcIWescPZR",
+                          "VeqlKVXnbCPyjZK",
+                          "ZgVqKaCOgYZtlZj",
+                          "hMFJKfVwkgapsNe",
+                          "hQxZXAecosoXsif",
+                          "kFGJHlrcwnWORzL",
+                          "nQdyNhxIKLmpZHk",
+                          "oHUgeTNOYNQdkGo",
+                          "wnrIZYwfVHPoLcU"
+            ],
+                  "refreshTokenValiditySeconds": 165274901,
+          "secretRequired": true
+                },
+            {
+                  "accessTokenValiditySeconds": 1725247274,
+                  "allowedScopes": [
+                        "DniUNWUNDSoDBmh",
+                        "PCKoRyrTHZkdjjQ",
+                        "TDGLJwfbUeziRgS",
+                        "WLaCWUpQQjadDze",
+                        "bIKJzzITpzkTKKe",
+                        "kBnuzmYirIFSDHb",
+                        "kllRvIwJInmGGDo",
+                        "sAGFquVxtUfsHkv",
+                        "vwHipQUTSpvwihE",
+                        "wtOOOSLDvdPUbpe"
+                      ],
+                  "autoApprove": false,
+                  "clientSecret": "$2a$10$JwbbZXMdlUcA1MLFuuVhIuGoNuWonOmBsS/rwU5MsehYhuYNATYlK",
+                  "contactEmail": "LwjNwwogIBxMbSj",
+                  "contactFirstName": "oZxnuPyIDpWMlif",
+                  "contactLastName": "SeJEOGhoEjVaEuk",
+                  "description": "neeqtcvQabZRfXP",
+                  "grantTypes": [
+                        "GcpIbfNNisoSNOz",
+                        "JjPlzVVjfDyupEg",
+                        "KWRiUwKAMugbfPu",
+                        "KgcbziOdPPRStin",
+                        "MgDYVCVVAVZjVCv",
+                        "RgVWYlRkucVyeYj",
+                        "WlPOmAIlNKbiLLh",
+                        "ngKPorkQQeZCDCh",
+                        "pXpajHeVaiBgnRy",
+                        "vzEJhYIqrlaVCSQ"
+                      ],
+                  "id": "ec9aa5b3-ac7c-45b1-b953-cedb81e5edc1",
+                  "name": "kttnlBIPLEnuBiZ",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+                  "redirectUris": [
+                        "BaRiVtqyuNMZHPN",
+                        "FwewjwPbonGsHGT",
+                        "NuJLdsXCsPHpohJ",
+                        "aNgUAnBjTypdOSR",
+                        "eBbFcnqrkiOrmFS",
+                        "eDuAcRFfkLHWLDQ",
+                        "gCsvfvXcBMxmweb",
+                        "iWNmdhYHPugfbuK",
+                        "wNIkiYFWKrEAkWx",
+                        "zzSSwkPFznXoTyS"
+                      ],
+                  "refreshTokenValiditySeconds": 1933494902,
+                  "secretRequired": true
+              },
+            {
+                  "accessTokenValiditySeconds": 0,
+                  "allowedScopes": [
+            
+                      ],
+                  "autoApprove": false,
+                  "clientSecret": null,
+                  "grantTypes": [
+            
+                      ],
+                  "id": "29b16b57-8212-4aeb-a180-b021399c0012",
+                  "privileges": [
+            
+                      ],
+                  "redirectUris": [
+            
+                      ],
+                  "refreshTokenValiditySeconds": 0,
+                  "secretRequired": false
+              },
+            {
+                  "accessTokenValiditySeconds": 600000,
+                  "allowedScopes": [
+                        "Authentication"
+                      ],
+                  "autoApprove": false,
+                  "clientSecret": "$2a$10$tMneESmkGPUwIQlzN3C4J.wcKrSmIq2uzsy96iKrUSOt42zNUZmNi",
+                  "grantTypes": [
+                        "client_credentials",
+                        "password"
+                      ],
+                  "id": "5139950a-ecc8-40e3-aa31-cf79fe1bb72a",
+                  "name": "Bootstrapped IDM Super Admin Client",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+                  "redirectUris": [
+            
+                      ],
+                  "refreshTokenValiditySeconds": 0,
+                  "secretRequired": false
+              },
+            {
+                  "accessTokenValiditySeconds": 1809947019,
+                  "allowedScopes": [
+                        "HTjyKASlWYCBEUL",
+                        "LuRbWINFCYYfqmn",
+                        "NISLZYXBQfuvomi",
+                        "aYHzRfYrLXanoHS",
+                        "evdIxHlemfODOKz",
+                        "iyDRsGFHXDfFBAV",
+                        "jFEaMGEnlgFXsoa",
+                        "mRAAkSlNbWWTqsG",
+                        "pDaAdfKPCVfbyaw",
+                        "vrdvokfjVROZPan"
+                      ],
+                  "autoApprove": false,
+                  "clientSecret": "$2a$10$b7MNfMwh0BGVubuKYlDbV.5oCqJoeQYG2hSo9C4DihXOeb4CXqrsG",
+                  "contactEmail": "VaMZTtqRqBvOvBl",
+                  "contactFirstName": "ffHtrxjHpzIAihS",
+                  "contactLastName": "VcaCBwtCeZCfsxb",
+                  "description": "KQlCEHKgKGObNhi",
+                  "grantTypes": [
+                        "GKZMjJbGxVvKdTf",
+                        "HTBnDSaTnuDootw",
+                        "IOgFyTrlNyQcYUa",
+                        "SNqWULQeMVLrPjN",
+                        "ZvEUOgyqMOeoSjW",
+                        "bBzeQzHXRvcUBPu",
+                        "bkCouMGlhCaDhgb",
+                        "iwUvuqgFKLfcDVh",
+                        "juyotxqlMIGwiys",
+                        "sgyYQrnkPRVclVJ"
+                      ],
+                  "id": "abe5645a-b303-4dd7-bc4c-5a3599e4ce72",
+                  "name": "uHZUxyJqDAyNptF",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+                  "redirectUris": [
+                        "GoAuyKzABYDTUqg",
+                        "HTwMUvPcQOYPkUT",
+                        "MJHipgdmPTxfUuj",
+                        "NLulSzXmjnCjifO",
+                        "VSlGAUERZQztLyT",
+                        "VwnhszQuaLEkQon",
+                        "bQJsqttVsvaHrtX",
+                        "cjMrHahYtcbTjkG",
+                        "oTZYTlcMuFlAnjY",
+                        "vnlzNRecpUiGAxX"
+                      ],
+                  "refreshTokenValiditySeconds": 1013769012,
+                  "secretRequired": false
+              },
+            {
+                  "accessTokenValiditySeconds": 2124378825,
+                  "allowedScopes": [
+                        "IYULWZOlSmyOuQO",
+                        "JmpNgONMIZWZQLp",
+                        "RmIgJyXmUUzIspF",
+                        "bogyoaCJjLeRUtC",
+                        "iPZCIbXkTgsRtes",
+                        "kihJzBSfXirxlxq",
+                        "pZuBJSjRtJYbVPX",
+                        "qNFJwRqvzogwQjb",
+                        "rXSYjJOocszzdTJ",
+                        "sUGVsVEAJADDenq"
+                      ],
+                  "autoApprove": false,
+                  "clientSecret": "$2a$10$b7UeYkUlzJtMQolYT0EfUuFvc0oxkWDIZK47WgIZl5WIs0fHe06jW",
+                  "contactEmail": "zfKuMYNkolWqAPK",
+                  "contactFirstName": "lgNQHrcREakjEDA",
+                  "contactLastName": "DILHlcDnOYXAxVh",
+                  "description": "fXFARwCruCXTNuE",
+                  "grantTypes": [
+                        "IUnJVmlnTYKcaJS",
+                        "SvKDbcqJkhyIhVX",
+                        "WDmeMkrVLQVxoUU",
+                        "apelxyjDDWXuCag",
+                        "dXivSOKqrFpaNAT",
+                        "mSJhWHXtVTkgqZS",
+                        "oQGyIGTwqCuirCf",
+                        "uTFTiJoIkarfpSA",
+                        "wQLNARxYjZmKDvE",
+                        "wuVbAPumkrvOlRv"
+                      ],
+                  "id": "00000000-0000-0000-0000-000000000000",
+                  "name": "EAtjINlTAReePqR",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+                  "redirectUris": [
+                        "BNsvZRCMNyrtJaD",
+                        "FQXBHSNdEUimaNJ",
+                        "GWsniUCqOoNSPhn",
+                        "IbBIvORNmaAFOFT",
+                        "VPLtuVKbdigjZfq",
+                        "VUDiKjFEYBHhiqC",
+                        "aCbOaeXgMEapzyi",
+                        "cOAMxaavBWeBbvd",
+                        "rCRustlayuLmXnf",
+                        "zPtYmXtjrNdJoRw"
+                      ],
+                  "refreshTokenValiditySeconds": 419507870,
+                  "secretRequired": true
+              },
+            {
+                  "accessTokenValiditySeconds": 754238008,
+                  "allowedScopes": [
+                        "AGxOUOfBzAibEnX",
+                        "DnXPgPkHcXDJSNx",
+                        "GDebGtpNFedFMPm",
+                        "KjpNccNvtfIRMqx",
+                        "OrnXWhKCFwFDVCf",
+                        "OtVrUtgbUMhnHQd",
+                        "aErDlnPkIolnFvG",
+                        "bwWHWGNsHlSZDrG",
+                        "obStsABdnutCNuT",
+                        "rVZzTsgYuSqsEym"
+                      ],
+                  "autoApprove": true,
+                  "clientSecret": "$2a$10$m7Wy0xvy7Cw2uF28XcWUmOpFMp4iYFZTYQexd5r3FKTX2Vx8RaeGu",
+                  "contactEmail": "WcYFsLhijGBPjQY",
+                  "contactFirstName": "MCeAUJhvcnmEXko",
+                  "contactLastName": "jALQvTAoYrFAvCI",
+                  "description": "JSxfCILtMXellpE",
+                  "grantTypes": [
+                        "AUPLnUKJczywqLT",
+                        "MvgXIcoULCbArnF",
+                        "SUTZcNpCNUryhPk",
+                        "SZzixGZwOOwsdEm",
+                        "TUwlKSJzOQJjFtE",
+                        "UBYbkDYVWUqXtgV",
+                        "UMvtOwECDAtRaKe",
+                        "bvVjUGMHZuOjFvH",
+                        "lRpGIaMPiVPcQaX",
+                        "skvQACLVEhUWetK"
+                      ],
+                  "id": "25eeb46b-0332-4954-923c-6cacfab61e5e",
+                  "name": "cztobBVnSsdhUos",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+                  "redirectUris": [
+                        "CoXWoFdJnvHsgEs",
+                        "LMyTEdLbEJGQjtO",
+                        "WRLrPPjoXxQhrnM",
+                        "fITktVzrQkgdBPx",
+                        "hgoSWAfeVATseYr",
+                        "pOEcevjMzzmFmaD",
+                        "qsCHHuCOZdZhkMh",
+                        "uKlmmfIzLBTmbLl",
+                        "wshXTzgJHAakLno",
+                        "ztSBSqnMvszorzj"
+                      ],
+                  "refreshTokenValiditySeconds": 2122765226,
+                  "secretRequired": false
+              },
+            {
+                  "accessTokenValiditySeconds": 112231087,
+                  "allowedScopes": [
+                        "JGMMpPRhdYuWXWM",
+                        "SqgATtdjuWzdIVM",
+                        "WdmJIkwNtFkpvxv",
+                        "cMKgMHLGHuqqebY",
+                        "iivGvqdAzwWBFgG",
+                        "lSYnBbQLOzEWUpj",
+                        "pPYAdhBUCTjGQMH",
+                        "psAmNOXdhwZxnJk",
+                        "sfinhnHbsSGRFOZ",
+                        "uHqjAJxKEWirNKZ"
+                      ],
+                  "autoApprove": false,
+                  "clientSecret": "$2a$10$bVZCH/ltnO2yuJLOIJnxoOJtDIKg/mDPT1ntIfUiD.mD3tS8GD/9.",
+                  "contactEmail": "BTzlOzxviOBjCVj",
+                  "contactFirstName": "weBRrYFUYrqXvEv",
+                  "contactLastName": "BEMvDYebZtccAUc",
+                  "description": "zVRyZGuFIxnBgJx",
+                  "grantTypes": [
+                        "FKkchfLYgGfpchA",
+                        "KAPCTXcUxFXwQOG",
+                        "KecdGwJMtQHpKAo",
+                        "PkyfhJsWTIThsiZ",
+                        "ScNivDlmQzDFXvx",
+                        "SckEYPiRzhuHeUo",
+                        "WCpeXjwnikZYRow",
+                        "ZzFcvrbMYryaIFn",
+                        "tKIHJZtIJYZhgCt",
+                        "thNSNRBUpckNMmn"
+                      ],
+                  "id": "4978ec48-abfb-47aa-854f-0e6410230c82",
+                  "name": "VJUpcLuNAFKgchn",
+                  "privileges": [
+                        "CHECK_TOKEN",
+                        "MANAGE_CLIENT",
+                        "MANAGE_IDP",
+                        "MANAGE_USER"
+                      ],
+                  "redirectUris": [
+                        "DIpcrCYZDvdYePH",
+                        "HkudJidqIdMkftV",
+                        "ZlkEKMJRhgwvYdJ",
+                        "acSmGqIydkSwxsO",
+                        "gPZTJxjtKZsbqjR",
+                        "geUrWuiXpMfamxo",
+                        "rxrFHgktNHVaRsx",
+                        "sKhhRKcdTqhOmiz",
+                        "vVjkirLiAjfqAXj",
+                        "wPvvwMlZjkExsZG"
+                      ],
+                  "refreshTokenValiditySeconds": 52955175,
+                  "secretRequired": true
+        }
+      ]''')
     }
   }
 
--- src/test/java/com/github/uglyog/pactgradletest/PactJUnitTest.java
+++ src/test/java/com/github/uglyog/pactgradletest/PactJUnitTest.java
@@ -3,6 +3,7 @@ package com.github.uglyog.pactgradletest;
 import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.loader.PactBrokerAuth;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
@@ -10,9 +11,9 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(PactRunner.class)
-@Provider("Activity Service")
-@PactBroker(host = "${pactbroker.url:localhost}", port = "8080", tags = {"prod", "dev"})
-@Ignore
+@Provider("Activity Service2")
+@PactBroker(host = "${pactbroker.url:localhost}", port = "1234", tags = {"latest", "prod", "dev"},
+  failIfNoPactsFound = false, authentication = @PactBrokerAuth(username = "test", password = "test"))
 public class PactJUnitTest {
 
   @TestTarget
