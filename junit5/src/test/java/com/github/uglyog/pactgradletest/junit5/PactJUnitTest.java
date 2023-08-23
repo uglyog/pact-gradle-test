b1e445b4de467e0bc228fb96b9499b30162db82e
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-08-23 11:00:09 +1000

    chore: Update to pact-jvm 4.6.2

--- junit5/src/test/java/com/github/uglyog/pactgradletest/junit5/PactJUnitTest.java
+++ junit5/src/test/java/com/github/uglyog/pactgradletest/junit5/PactJUnitTest.java
@@ -1,23 +1,30 @@
 package com.github.uglyog.pactgradletest.junit5;
 
+import au.com.dius.pact.core.pactbroker.ConsumerVersionSelectors;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
-import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
-import au.com.dius.pact.provider.junitsupport.loader.VersionSelector;
+import au.com.dius.pact.provider.junitsupport.loader.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Provider("Activity Service")
-@PactBroker(url = "https://test.pact.dius.com.au",
-  authentication = @PactBrokerAuth(username = "dXfltyFMgNOFZAxr8io9wJ37iUpY42M", password = "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"),
-  consumerVersionSelectors = { @VersionSelector(consumer = "Foo Web Client", tag = "tag/with/slash") }
+@PactBroker(url = "https://test.pactflow.io",
+  authentication = @PactBrokerAuth(username = "dXfltyFMgNOFZAxr8io9wJ37iUpY42M", password = "O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1")
 )
 public class PactJUnitTest {
+
+
+  @PactBrokerConsumerVersionSelectors
+  public static List<ConsumerVersionSelectors.Selector> selectors() {
+    return Arrays.asList(new ConsumerVersionSelectors.Selector("tag/with/slash", true, "Foo Web Client"));
+  }
 
   @BeforeEach
   void setup(PactVerificationContext context) {
